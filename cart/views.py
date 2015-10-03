0789272c280e426cb8adb7782c93f0d7366cfb7b
Navjot Singh <weavebytes@gmail.com>
2015-10-03 16:17:29 +0530

    added improts

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,15 @@
+import traceback
+
 from django.shortcuts import render
+from django.http import *
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from cart.models import *
+from dashboard.forms import LoginForm
+from django.core.context_processors import csrf
 
 @login_required
 def home(request):
