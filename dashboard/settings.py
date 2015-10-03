5e06f32e20688caa9d1f7b4699a1085aebef4044
Navjot Singh <weavebytes@gmail.com>
2015-10-03 15:53:26 +0530

    updated db settings

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djcart',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
