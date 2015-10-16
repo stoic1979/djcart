a8f5cbe9270c10ec66d50db7318cfa0f7acfce3b
Navjot Singh <weavebytes@gmail.com>
2015-10-16 23:37:52 +0530

    added cart

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration'
+    'registration',
+    'cart'
 )
 
 MIDDLEWARE_CLASSES = (
