716b4c7d20d2b60979f217ce145ab48ba534726f
Navjot Singh <weavebytes@gmail.com>
2015-10-17 00:02:29 +0530

    updated cart models/admin

--- cart/admin.py
+++ cart/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from cart.models import *
 
-admin.site.register(Project)
-admin.site.register(Task)
-admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Currency)
+admin.site.register(Offer)
+admin.site.register(Unit)
+admin.site.register(Product)
+admin.site.register(Property)
+admin.site.register(Review)
